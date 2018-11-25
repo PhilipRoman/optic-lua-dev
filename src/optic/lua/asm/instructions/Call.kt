fc4f08cf1c148d7be2d70793f5d7a04ebc7370f2
Philip Roman <dev.philipjohn@gmail.com>
2018-11-25 12:00:42 +0200

    Cleanup

--- src/optic/lua/asm/instructions/Call.kt
+++ src/optic/lua/asm/instructions/Call.kt
@@ -1,13 +1,11 @@
 package optic.lua.asm.instructions
 
 import optic.lua.asm.Register
-import optic.lua.asm.RegisterFactory
 import optic.lua.asm.Step
 
 class Call(val function: Register, val args: List<Register>, val output: Register) : Step {
     override fun toString(): String {
-        val resultUnused = output == RegisterFactory.unused()
-        return if (resultUnused)
+        return if (output.isUnused())
             "call $function($args)"
         else
             "call $output = $function($args)"
