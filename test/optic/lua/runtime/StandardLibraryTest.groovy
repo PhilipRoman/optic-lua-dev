d97033ebc00112c24233fb00bd68273b83abd40e
Philip Roman <dev.philipjohn@gmail.com>
2018-12-23 14:17:04 +0200

    Added empty test stubs for standard library

--- test/optic/lua/runtime/StandardLibraryTest.groovy
+++ test/optic/lua/runtime/StandardLibraryTest.groovy
@@ -4,4 +4,19 @@ import groovy.transform.CompileStatic
 
 @CompileStatic
 class StandardLibraryTest extends GroovyTestCase {
+    void testToString() {
+        // TODO
+    }
+
+    void testToNumber() {
+        // TODO
+    }
+
+    void testTableConcat() {
+        // TODO
+    }
+
+    void testPrint() {
+        // TODO
+    }
 }
