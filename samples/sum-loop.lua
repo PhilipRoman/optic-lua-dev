2d29b41867d988de683ac13c278849206681cca2
Philip Roman <dev.philipjohn@gmail.com>
2018-12-24 15:52:10 +0200

    Added benchmark program

--- samples/sum-loop.lua
+++ samples/sum-loop.lua
@@ -1,4 +1,6 @@
-local N = 5
+local time = os.time
+local start = time()
+local N = 10000
 local sum = 0
 for i = 1, N do
     for j = 1, N do
@@ -6,3 +8,4 @@ for i = 1, N do
     end
 end
 print(sum)
+print("TIME: " .. (time() - start))
