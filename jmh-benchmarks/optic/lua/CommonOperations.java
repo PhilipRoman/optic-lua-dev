57b2b653f22739695ac1f4c4b0d74b6e6edad08d
Philip Roman <dev.philipjohn@gmail.com>
2019-01-18 20:03:24 +0200

    Added more JMH benchmarks for common operations

--- jmh-benchmarks/optic/lua/CommonOperations.java
+++ jmh-benchmarks/optic/lua/CommonOperations.java
@@ -13,10 +13,17 @@ import java.io.IOException;
 public class CommonOperations {
 	private static final LuaTable TABLE = TableOps.create("foo", "bar");
 	private static final LuaContext CONTEXT = LuaContext.create();
-	private static final LuaFunction FUNCTION = new LuaFunction() {
+	private static final LuaFunction DYNAMIC_SUM_FUNCTION = new LuaFunction() {
 		@Override
 		public Object[] call(LuaContext context, Object... args) {
 			return ListOps.create(DynamicOps.add(context, ListOps.get(args, 0), ListOps.get(args, 1)));
+		}
+	};
+
+	private static final LuaFunction DYNAMIC_EMPTY_FUNCTION = new LuaFunction() {
+		@Override
+		public Object[] call(LuaContext context, Object... args) {
+			return ListOps.empty();
 		}
 	};
 
@@ -36,8 +43,23 @@ public class CommonOperations {
 
 	@SuppressWarnings("ConstantConditions")
 	@Benchmark
-	public static double functionCall() {
-		return StandardLibrary.toNumber(ListOps.get(FunctionOps.call(FUNCTION, CONTEXT, 2, 3), 0));
+	public static double functionCallSum() {
+		return StandardLibrary.toNumber(ListOps.get(FunctionOps.call(DYNAMIC_SUM_FUNCTION, CONTEXT, 2, 3), 0));
+	}
+
+	@Benchmark
+	public static void functionCallSumDiscard() {
+		FunctionOps.call(DYNAMIC_SUM_FUNCTION, CONTEXT, 2, 3);
+	}
+
+	@Benchmark
+	public static void functionCallEmpty() {
+		FunctionOps.call(DYNAMIC_EMPTY_FUNCTION, CONTEXT);
+	}
+
+	@Benchmark
+	public static void functionCallEmptyWithParameters() {
+		FunctionOps.call(DYNAMIC_EMPTY_FUNCTION, CONTEXT, 1, 2, 3);
 	}
 
 	public static void main(String[] args) throws IOException, RunnerException {
