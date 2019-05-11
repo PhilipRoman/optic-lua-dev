425ec855f04da285e7ed9cfea71d9f1213314187
Philip Roman <dev.philipjohn@gmail.com>
2019-05-11 11:27:06 +0300

    Modified 'electric-fields' test

--- integration-test/optic/lua/ElectricFieldsTest.groovy
+++ integration-test/optic/lua/ElectricFieldsTest.groovy
@@ -6,6 +6,6 @@ import groovy.transform.CompileStatic
 class ElectricFieldsTest extends GroovyTestCase {
     void testElectricFields() {
         def program = new SampleProgram("samples/electric-fields.lua")
-        assertEquals(program.run().first().toDouble(), 0.0011350634653437934d, 1e-10d)
+        assertEquals(program.run().first().toDouble(), 0.00079832819298365d, 1e-15d)
     }
 }
--- samples/electric-fields.lua
+++ samples/electric-fields.lua
@@ -1,47 +1,44 @@
 local fields = {
     { x = 23, y = 78, charge = 0.32 },
-    { x = 123, y = 578, charge = 0.33 },
-    { x = 223, y = 678, charge = 0.34 },
-    { x = 323, y = 778, charge = 0.35 },
+    { x = 123, y = 578, charge = 1.33 },
+    { x = 223, y = 378, charge = 0.34 },
+    { x = 323, y = 228, charge = 1.35 },
     { x = 423, y = 478, charge = 0.36 },
-    { x = 523, y = 178, charge = 0.37 },
-    { x = 623, y = 278, charge = 0.38 },
-    { x = 723, y = 378, charge = 0.39 },
-    { x = 823, y = 878, charge = 0.40 },
-    { x = 923, y = 978, charge = 0.41 },
+    { x = 523, y = 178, charge = 1.37 },
+    { x = 473, y = 278, charge = 0.38 },
+    { x = 373, y = 328, charge = 1.39 },
+    { x = 173, y = 428, charge = 0.40 },
+    { x = 273, y = 128, charge = 1.41 },
 }
 
-local hash = 0
-local function use(x)
-    hash = (hash + x) * 0.99
-end
+local WIDTH, HEIGHT = 500, 500
 
-local WIDTH, HEIGHT = 1000, 500
+local matrix = {}
 
-local math = math
-local atan2 = math.atan2
-local sin = math.sin
-local cos = math.cos
-local sqrt = math.sqrt
-
-for x = 0, WIDTH do
+for x = 1, WIDTH do
+    matrix[x] = {}
     for y = 1, HEIGHT do
         local vx, vy = 0, 0
-        for i = 1, #fields do
-            local field = fields[i]
+        for _, field in ipairs(fields) do
             local dx, dy = x - field.x, y - field.y
-            local angle = atan2(dy, dx)
-            local distance = sqrt(dx * dx + dy * dy)
+            local angle = math.atan2(dy, dx)
+            local distance = math.sqrt(dx ^ 2 + dy ^ 2)
             if distance == 0 then
-                distance = 0.00001
+                distance = 1
             end
             local intensity = field.charge / distance / distance
-            vx = vx + cos(angle) * intensity
-            vy = vy + sin(angle) * intensity
+            vx = vx + math.cos(angle) * intensity
+            vy = vy + math.sin(angle) * intensity
         end
-        local length = sqrt(vx * vx + vy * vy)
-        use(length)
+        matrix[x][y] = { vx, vy }
     end
 end
 
-print(hash)
+local sum = 0
+for x = 1, WIDTH do
+    for y = 1, HEIGHT do
+        local vector = matrix[x][y]
+        sum = sum + math.sqrt(vector[1] ^ 2 + vector[2] ^ 2)
+    end
+end
+print(sum / (WIDTH * HEIGHT))
