7aca33ea9ac1b6b2aff133b8938084f53422cd14
Philip Roman <dev.philipjohn@gmail.com>
2019-05-24 23:11:37 +0300

    Fix bytecode compiler test

--- test/optic/lua/io/JavaToBytecodeCompilerTest.groovy
+++ test/optic/lua/io/JavaToBytecodeCompilerTest.groovy
@@ -10,14 +10,14 @@ public class Foo {
     public void method() {
         System.out.println("Hello!");
     }
-}""")
+}""").values().first()
         assert bytecode.length > 100
         assert bytecode[0..3] == [(byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE]
 
 
         def otherBytecode = new JavaToBytecodeCompiler().compile("""
 public class Foo {
-}""")
+}""").values().first()
         assert otherBytecode[0..3] == [(byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE]
         assert otherBytecode.length < bytecode.length
 
