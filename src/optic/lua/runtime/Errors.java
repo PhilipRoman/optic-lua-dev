b36708af22e055ab229260d9971308d820817495
Philip Roman <dev.philipjohn@gmail.com>
2019-05-25 15:28:10 +0300

    Return exception from Errors instead of throwing it

--- src/optic/lua/runtime/Errors.java
+++ src/optic/lua/runtime/Errors.java
@@ -4,12 +4,12 @@ import org.jetbrains.annotations.Contract;
 
 final class Errors {
 	public static RuntimeException argument(int n, String expected) {
-		throw new IllegalArgumentException("Bad parameter #" + n + " (" + expected + " expected)");
+		return new IllegalArgumentException("Bad parameter #" + n + " (" + expected + " expected)");
 	}
 
 	public static RuntimeException attemptTo(String action, Object func) {
 		String type = StandardLibrary.type(func);
 		String explanation = type.equals("userdata") ? " (" + func.getClass().getCanonicalName() + ')' : "";
-		throw new IllegalArgumentException("attempt to " + action + " a " + type + " value" + explanation);
+		return new IllegalArgumentException("attempt to " + action + " a " + type + " value" + explanation);
 	}
 }
