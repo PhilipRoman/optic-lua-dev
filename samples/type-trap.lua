00594c958f18e858743a1f2b18181ddc93845335
PhilipRoman <dev.philipjohn@gmail.com>
2019-06-27 14:11:37 +0300

    Improve 'type-trap' test

--- samples/type-trap.lua
+++ samples/type-trap.lua
@@ -5,5 +5,20 @@ end)(1) -- type = object
 
 for i = 1, 5 do
     print(number + 1) -- only first iteration may use primitive addition
-    number = number + dynamic
+    number = number + dynamic -- number should become 'object'
+end
+
+local root = 1
+if true then
+    local branch_1 = root + 3
+    local branch_2 = root + 2.5
+    root = root + 2.5
+    assert(branch_1 - 0.5 == branch_2)
+end
+root = 1
+for i = 1, 5 do
+    local branch_1 = root + 3
+    local branch_2 = root + 2.5
+    root = root + 2.5
+    assert(branch_1 - 0.5 == branch_2)
 end
